95c03f2ae89c1d449acbdc2fc57cd7b5c0a5beb0
Scott Dugas <scott.dugas@gmail.com>
2012-01-11 00:22:26 -0500

    Added initial implementation for kata 4 & tests
    This doesn't actually work, but I'm not sure why, when I call map it
    throws an exception about the stream already being closed, but doseq
    works just fine.

--- src/kata/kata4.clj
+++ src/kata/kata4.clj
@@ -1,1 +1,27 @@
-(ns kata.kata4)
+(ns kata.kata4
+  (:use [clojure.java.io :only (reader)]))
+
+(defn firstthree [text]
+  (map (fn [s] (Integer/parseInt s))
+       (take 3 (re-seq #"[0-9]+" text)))
+  )
+
+(defn spread [first]
+  (if (< (count first) 3)
+    nil
+   (- (nth first 2) (nth first 1)))
+  )
+
+(defn spreadtext [text]
+  (println (firstthree text))
+  (spread (firstthree text)))
+
+(defn temp []
+  (with-open [rdr (reader "weather.dat")]
+    (map (fn [l] (println l)) (line-seq rdr))
+
+
+;;    (doseq [l (line-seq rdr)]
+      ;;      (when (.contains l "MxT") (println l)) )
+      )
+    )
--- test/kata/test/kata4.clj
+++ test/kata/test/kata4.clj
@@ -1,3 +1,21 @@
 (ns kata.test.kata4
   (:use [kata.kata4])
   (:use [clojure.test]))
+
+(deftest testread
+  (is (= 30 (temp))))
+
+(deftest firstthreetest
+  (are [expect text] (= expect (firstthree text))
+       [2 63 63] "   2  63    63    71          46.5       0.00         330  8.7 340  23  3.3  70 28 1004.5"
+       [5 63 64] "   5  63    64    71          46.5       0.00         330  8.7 340  23  3.3  70 28 1004.5"
+       [7 63 65] "   7  63    65    71          46.5       0.00         330  8.7 340  23  3.3  70 28 1004.5"
+       [12 20 35] "   12  20    35    71          46.5       0.00         330  8.7 340  23  3.3  70 28 1004.5"))
+
+
+(deftest testspread
+  (are [expect text] (= expect (spread text))
+       0 [2 63 63]
+       1 [2 63 64]
+       2 [2 63 65]
+       15 [2 20 35]))
