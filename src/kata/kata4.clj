bec5d8c91c1f4d3bc69fa091e3f7596ffc0b6fcb
Scott Dugas <scott.dugas@gmail.com>
2012-01-11 20:35:57 -0500

    added reduce call
    with map it didn't actually evaluate until well after the with-open
    block, thus causing an exception. This still doesn't work, because
    numbers suck, and so does inconsistent data

--- src/kata/kata4.clj
+++ src/kata/kata4.clj
@@ -2,23 +2,30 @@
   (:use [clojure.java.io :only (reader)]))
 
 (defn firstthree [text]
-  (map (fn [s] (Integer/parseInt s))
-       (take 3 (re-seq #"[0-9]+" text)))
+  (map (fn [s] (rationalize (Double/parseDouble s)))
+       (take 3 (re-seq #"[0-9]+(?:\.[0-9]+)?" text)))
   )
 
-(defn spread [first]
-  (if (< (count first) 3)
+(defn spread [three]
+  (if (< (count three) 3)
     nil
-   (- (nth first 2) (nth first 1)))
+    [(first three) (-' (nth three 1) (nth three 2))])
   )
 
 (defn spreadtext [text]
   (println (firstthree text))
   (spread (firstthree text)))
 
+(defn lesser [a b]
+  (cond
+   (nil? b) a
+   (nil? a) b
+   (< (second a) (second b)) a
+   true b))
+
 (defn temp []
   (with-open [rdr (reader "weather.dat")]
-    (map (fn [l] (println l)) (line-seq rdr))
+    (reduce lesser (map spreadtext (line-seq rdr)))
 
 
 ;;    (doseq [l (line-seq rdr)]
