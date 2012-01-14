4ed525d4da8098225323240891a17e32b1b10dab
Scott Dugas <scott.dugas@gmail.com>
2012-01-14 16:53:01 -0500

    Added code to create an array from a word using md5

--- src/kata/kata5.clj
+++ src/kata/kata5.clj
@@ -1,8 +1,25 @@
 (ns kata.kata5
   (:use [clojure.java.io :only (reader)]))
 
+(defn single-hash [text]
+  (first (.digest (java.security.MessageDigest/getInstance "md5")
+                  (.getBytes text))
+         )
+  )
+
+(defn nums []
+  (range 0 256))
+
+(defn create-hash-bit [word i]
+  (= 1 (mod (single-hash (str i word i)) 2)))
+
+
+(defn hash-array [word]
+  (map #(create-hash-bit word %) (nums))
+  )
+
 (defn bloom-hash [a b]
-  a)
+  (hash-array a))
 
 
 (defn create-bloom []
--- test/kata/test/kata5.clj
+++ test/kata/test/kata5.clj
@@ -2,6 +2,9 @@
   (:use [kata.kata5])
   (:use [clojure.test]))
 
+(deftest test-ha
+  (is (= 3 (hash-array "box")))
+  (is (= 3 (hash-array "fish"))))
 
 (deftest something
   (is (= 3 (create-bloom))))
